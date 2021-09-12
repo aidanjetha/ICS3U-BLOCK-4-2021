2f2d3724fae0b088c221be676f8d5a2589cff64e
Aidan Jetha <ajetha@bayviewglen.ca>
2021-09-12 18:26:08 -0400

    Homework

---
+++ UNIT ONE - INTRODUCTION TO COMPUTER SCIENCE/src/week1/Homework.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Homework {
+    public static void main(String[] args) {
+        
+        int firstValue = 13;
+        double secondValue = 18.5;
+        double sumOfValues = firstValue + secondValue;
+
+        System.out.println(sumOfValues);
+    }
+}
