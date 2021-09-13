c06e69e0fe47b9e7ecafa493a8a1cb292fdabe6b
Aidan Jetha <ajetha@bayviewglen.ca>
2021-09-13 14:35:02 -0400

    example 6

--- UNIT ONE - INTRODUCTION TO COMPUTER SCIENCE/src/week1/Homework.java
+++ UNIT ONE - INTRODUCTION TO COMPUTER SCIENCE/src/week1/Homework.java
@@ -1,12 +1,9 @@
 package week1;
 
-public class Homework {
+public class homework {
     public static void main(String[] args) {
         
-        int firstValue = 13;
-        double secondValue = 18.5;
-        double sumOfValues = firstValue + secondValue;
+        
 
-        System.out.println(sumOfValues);
     }
 }
